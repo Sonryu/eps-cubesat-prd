78f1bc80d05da3ddfc3dbe25405941e96dfb26af
Sonryu <watsonvilar@gmail.com>
2026-03-15 22:54:34 -0300

    EPS funcionando, falta testar

--- simulacao_cubesat/src/main.cpp
+++ simulacao_cubesat/src/main.cpp
@@ -9,39 +9,28 @@
 #define BATTERY_LOW       (1 << 1)
 #define BATTERY_CRITICAL  (1 << 2)
 
-EventGroupHandle_t EventGroup_BATERIA;
-SemaphoreHandle_t Mutex_BATERIA;
-TaskHandle_t controle_tarefa_Payload = NULL;
+EventGroupHandle_t batteryEventGroup;
+SemaphoreHandle_t batteryMutex;
+TaskHandle_t payloadTaskHandle = NULL;
 
-float nivel_BATERIA = 100.0; 
+float batteryLevel = 100.0; 
 
 // ======================
 // TAREFA: Monitor de Bateria
 // ======================
 void TaskBatteryMonitor(void *pvParameters) {
   while (true) {
-    xSemaphoreTake(Mutex_BATERIA, portMAX_DELAY);
-    nivel_BATERIA -= random(4, 8) * 0.5; // Simulação de descarga
-    if (nivel_BATERIA < 0) nivel_BATERIA = 0;
+    xSemaphoreTake(batteryMutex, portMAX_DELAY);
+    batteryLevel -= random(4, 8) * 0.5; // Simulação de descarga
+    if (batteryLevel < 0) batteryLevel = 0;
 
-    
-    
-    if( nivel_BATERIA >= 50.0 ){
-        xEventGroupSetBits(EventGroup_BATERIA, BATTERY_OK);
-    
-      } else if ( nivel_BATERIA >= 20.0 ) {
-        xEventGroupSetBits(EventGroup_BATERIA, BATTERY_LOW);
-    
-      } else {
-        xEventGroupSetBits(EventGroup_BATERIA, BATTERY_CRITICAL);
-    }
     // TODO 1: Limpar os bits antigos do EventGroup
     // TODO 2: Implementar a lógica de decisão:
     // Se > 50%: Setar BATTERY_OK
     // Se entre 20% e 50%: Setar BATTERY_LOW
     // Se < 20%: Setar BATTERY_CRITICAL
 
-    xSemaphoreGive(Mutex_BATERIA);
+    xSemaphoreGive(batteryMutex);
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
 }
@@ -52,18 +41,12 @@ void TaskBatteryMonitor(void *pvParameters) {
 void TaskTelemetry(void *pvParameters) {
   while (true) {
     // TODO 3: Obter o valor de 'batteryLevel' de forma segura
-      xSemaphoreTake(Mutex_BATERIA, portMAX_DELAY);
-    float level = nivel_BATERIA; // Substituir pela lógica correta
-      xSemaphoreGive(Mutex_BATERIA);
+    float level = 0; // Substituir pela lógica correta
+
     // TODO 4: Ler os bits atuais do EventGroup para exibir o estado
-      EventBits_t statusBits = xEventGroupGetBits(EventGroup_BATERIA);
     Serial.print("[TELEMETRIA] Bateria: ");
     Serial.print(level);
     Serial.println("%");
-      
-      if ( statusBits & BATTERY_OK ) Serial.print("OK");
-      else if ( statusBits & BATTERY_LOW ) Serial.print("LOW");
-      else if (statusBits & BATTERY_CRITICAL) Serial.print("CRITICAL"); 
 
     vTaskDelay(pdMS_TO_TICKS(3000));
   }
@@ -76,26 +59,12 @@ void TaskCritical(void *pvParameters) {
   while (true) {
     // TODO 5: Esperar por QUALQUER um dos bits (OK, LOW ou CRITICAL)
     // Usar xEventGroupWaitBits com lógica 'OU'
-    EventBits_t bits;
-    
-      bits = xEventGroupWaitBits(
-          EventGroup_BATERIA,
-          BATTERY_OK | BATTERY_LOW | BATTERY_CRITICAL,
-          pdFALSE,
-          pdFALSE,
-          portMAX_DELAY
-      );
+    EventBits_t bits; 
 
     // TODO 6: Implementar a reação do sistema:
     // Se BATTERY_CRITICAL: vTaskSuspend(payloadTaskHandle)
     // Se BATTERY_OK: vTaskResume(payloadTaskHandle)
-      if ( bits & BATTERY_CRITICAL ) {
-        Serial.print("! ALERTA ! :: Suspendendo Payload ! ");
-          vTaskSuspend(controle_tarefa_Payload);
-      
-      } else if ( bits & BATTERY_OK ) {
-          vTaskResume(controle_tarefa_Payload);
-      }
+
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
@@ -109,12 +78,12 @@ void TaskPayload(void *pvParameters) {
 
 void setup() {
   Serial.begin(115200);
-  EventGroup_BATERIA = xEventGroupCreate();
-  Mutex_BATERIA = xSemaphoreCreateMutex();
+  batteryEventGroup = xEventGroupCreate();
+  batteryMutex = xSemaphoreCreateMutex();
 
   xTaskCreatePinnedToCore(TaskBatteryMonitor, "BatMon", 4096, NULL, 3, NULL, 1);
   xTaskCreatePinnedToCore(TaskTelemetry, "Telem", 4096, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(TaskPayload, "Payload", 4096, NULL, 1, &controle_tarefa_Payload, 1);
+  xTaskCreatePinnedToCore(TaskPayload, "Payload", 4096, NULL, 1, &payloadTaskHandle, 1);
   xTaskCreatePinnedToCore(TaskCritical, "Critical", 4096, NULL, 3, NULL, 1);
 }
 
